5b489bf566a0c240eda06669bf349149ca6679b4
Donovan Daniels <hewwo@yiff.rocks>
2021-10-02 17:16:07 -0500

    randomize passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
 require "digest/md5"
 require "net/http"
 require "tempfile"
+require "securerandom"
 
 unless Rails.env.test?
   puts "== Creating elasticsearch indices ==\n"
@@ -15,22 +16,26 @@ puts "== Seeding database with sample content ==\n"
 # Uncomment to see detailed logs
 #ActiveRecord::Base.logger = ActiveSupport::Logger.new($stdout)
 
+admin_password = SecureRandom.hex(20)
+system_password = SecureRandom.hex(20)
+File.write(Rails.root.join("db", "admin.pwd"), admin_password)
+File.write(Rails.root.join("db", "system.pwd"), system_password)
 admin = User.find_or_create_by!(name: "admin") do |user|
   user.created_at = 2.weeks.ago
-  user.password = "e621test"
-  user.password_confirmation = "e621test"
+  user.password = admin_password
+  user.password_confirmation = admin_password
   user.password_hash = ""
-  user.email = "admin@e621.net"
+  user.email = "hewwo@yiff.rocks"
   user.can_upload_free = true
   user.can_approve_posts = true
   user.level = User::Levels::ADMIN
 end
 
 User.find_or_create_by!(name: Danbooru.config.system_user) do |user|
-  user.password = "ae3n4oie2n3oi4en23oie4noienaorshtaioresnt"
-  user.password_confirmation = "ae3n4oie2n3oi4en23oie4noienaorshtaioresnt"
+  user.password = system_password
+  user.password_confirmation = system_password
   user.password_hash = ""
-  user.email = "system@e621.net"
+  user.email = "system@e621.local"
   user.can_upload_free = true
   user.can_approve_posts = true
   user.level = User::Levels::JANITOR
@@ -43,7 +48,7 @@ end
 
 unless Rails.env.test?
   CurrentUser.user = admin
-  CurrentUser.ip_addr = "127.0.0.1"
+    CurrentUser.ip_addr = "127.0.0.1"
 
   resources = YAML.load_file Rails.root.join("db", "seeds.yml")
   url = "https://e621.net/posts.json?limit=100&tags=id:" + resources["post_ids"].join(",")
@@ -65,16 +70,16 @@ unless Rails.env.test?
 
     md5 = Digest::MD5.hexdigest(data)
     service = UploadService.new({
-                                    uploader_id: CurrentUser.id,
-                                    uploader_ip_addr: CurrentUser.ip_addr,
-                                    file: file,
-                                    tag_string: post["tags"].values.flatten.join(" "),
-                                    source: post["sources"].join("\n"),
-                                    description: post["description"],
-                                    rating: post["rating"],
-                                    md5: md5,
-                                    md5_confirmation: md5
-                                })
+        uploader_id: CurrentUser.id,
+        uploader_ip_addr: CurrentUser.ip_addr,
+        file: file,
+        tag_string: post["tags"].values.flatten.join(" "),
+        source: post["sources"].join("\n"),
+        description: post["description"],
+        rating: post["rating"],
+        md5: md5,
+        md5_confirmation: md5
+    })
 
     service.start!
   end
