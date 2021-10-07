280504075802b589e6304ae4e44adeba00ebc1d1
Donovan Daniels <hewwo@yiff.rocks>
2021-10-07 08:23:55 -0500

    postgres trusted subnet

--- script/install_prod/install.sh
+++ script/install_prod/install.sh
@@ -73,7 +73,7 @@ fi
 echo "Setting up postgres..."
 # allow connections from the host machine
 if ! grep -q "172" "/etc/postgresql/12/main/pg_hba.conf"; then
-  echo "host danbooru,danbooru2,danbooru3 danbooru 172.0.0.0/8 trust" >> /etc/postgresql/12/main/pg_hba.conf
+  echo "host danbooru,danbooru2,danbooru3 danbooru 172.99.0.0/24 trust" >> /etc/postgresql/12/main/pg_hba.conf
 fi
 # do not require passwords for authentication
 sed -i -e 's/md5/trust/' /etc/postgresql/12/main/pg_hba.conf
