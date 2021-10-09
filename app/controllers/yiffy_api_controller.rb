b79ad6033d5a5fd734fdce3970e358ed7cf5e04e
Donovan Daniels <hewwo@yiff.rocks>
2021-10-09 08:50:50 -0500

    render json

--- app/controllers/yiffy_api_controller.rb
+++ app/controllers/yiffy_api_controller.rb
@@ -1,12 +1,9 @@
 class YiffyApiController < ApplicationController
   def index
-    respond_to do |format|
-      format.json do
-        render json: {
-          type: params[:category],
-	      test: "e"
-        }.to_json
-      end
+      render json: {
+        type: params[:category],
+	     test: "e"
+      }.to_json
     end
   end
 end
