58d35acdcde8054f772107229d3e6488df63750d
Donovan Daniels <hewwo@yiff.rocks>
2021-10-09 12:55:29 -0500

    uh

--- config/routes.rb
+++ config/routes.rb
@@ -513,7 +513,7 @@ Rails.application.routes.draw do
   get "/V3/furry/yiff/:category" => "yiffy_api#yiff"
 
   # yiffy api (subdomain)
-  constraints subdomain: 'v3' do
+  constraints subdomain => 'v3' do
     get "/" => redirect("https://#{Danbooru.config.hostname}/help/api")
     get "/animals/:category" => "yiffy_api#animals"
     get "/furry/:category" => "yiffy_api#furry"
