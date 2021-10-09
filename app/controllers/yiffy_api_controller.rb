66ab0d79d43e1ae507edb1c2c323961f6f66fb7a
Donovan Daniels <hewwo@yiff.rocks>
2021-10-09 09:25:26 -0500

    API 404

--- app/controllers/yiffy_api_controller.rb
+++ app/controllers/yiffy_api_controller.rb
@@ -1,8 +1,22 @@
 class YiffyApiController < ApplicationController
+  class APIErrors
+    NOT_FOUND = {
+      code: 0,
+      message: "Not Found."
+    }
+  end
+
   def index
     render json: {
       type: params[:category],
-	  test: "e"
+	    test: "e"
+    }.to_json
+  end
+
+  def not_found
+    render json: {
+      success: false,
+      error: APIErrors::NOT_FOUND
     }.to_json
   end
 end
