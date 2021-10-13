95cc825d0190d2bf4b77847d3391b7a53be5c84e
Donovan Daniels <hewwo@yiff.rocks>
2021-10-13 16:05:24 -0500

    mm format

--- script/db_backup.sh
+++ script/db_backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-file="$(date +%F)_h$(date %H)_e621.sql"
+file="$(date +%F)_h$(date +%H)_e621.sql"
 mkdir -p $SCRIPT_DIR/../data/db_backup
 docker exec -u postgres e621 pg_dump e621 > $SCRIPT_DIR/../data/db_backup/$file
