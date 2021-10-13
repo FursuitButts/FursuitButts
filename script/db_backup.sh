9ac59f796d5e814069cb398b14979b60f821b588
Donovan Daniels <hewwo@yiff.rocks>
2021-10-13 15:59:28 -0500

    fucks sake

--- script/db_backup.sh
+++ script/db_backup.sh
@@ -2,5 +2,5 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 file="$(date +%F_%H-%M-%s_e621).sql"
-mkdir -p $SCRIPT_DIR/data/db_backup
+mkdir -p $SCRIPT_DIR/../data/db_backup
 docker exec -u postgres e621 pg_dump e621 > $SCRIPT_DIR/../data/db_backup/$file
