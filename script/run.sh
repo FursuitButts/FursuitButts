b884eaaa30dfddeaa801b4b181f1948841331adf
Donovan Daniels <hewwo@yiff.rocks>
2021-10-13 13:53:32 -0500

    owned

--- script/run.sh
+++ script/run.sh
@@ -5,13 +5,18 @@ PID_FILE=APP_DIR/tmp/pids/server.pid
 cd $APP_DIR
 sudo -i -u danbooru bash -c "/home/danbooru/ruby-setup.sh '$APP_DIR' '/etc/profile.d/chruby.sh'"
 $APP_DIR/sh/compile
+chown -R postgres:postgres /var/run/postgresql /var/lib/postgresql /etc/postgresql
+chown -R danbooru:danbooru $APP_DIR
+chown -R redis:redis /var/lib/redis
+
+
 service redis start
 service postgresql start
 service nginx start
 
 if [ -f $PID_FILE ]; then
     echo "Removing pid file.."
-    rm /home/danbooru/danbooru/tmp/pids/server.pid
+    rm $APP_DIR/tmp/pids/server.pid
 fi
 
 echo "Starting.."
