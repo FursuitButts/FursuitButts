41fcaf4b17181ddf8608caeff564ba20f226cb2d
Donovan Daniels <hewwo@yiff.rocks>
2021-10-13 15:56:47 -0500

    absolute dir

--- script/db_backup.sh
+++ script/db_backup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 file="$(date +%F_%H-%M-%s_e621).sql"
 mkdir -p data/db_backup
-docker exec -u postgres e621 pg_dump e621 > data/db_backup
+docker exec -u postgres e621 pg_dump e621 > $SCRIPT_DIR/../data/db_backup
