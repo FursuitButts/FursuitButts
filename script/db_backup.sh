c932958bf1135ede6e1a1bad81b87ce234b4ea45
Donovan Daniels <hewwo@yiff.rocks>
2021-10-16 06:02:50 -0500

    container name

--- script/db_backup.sh
+++ script/db_backup.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 file="$(date +%F)_h$(date +%H).sql"
 mkdir -p $SCRIPT_DIR/../data/db_backup
-docker exec -u postgres e621 pg_dump e621 > $SCRIPT_DIR/../data/db_backup/$file
+docker exec -u postgres yiffyapi pg_dump e621 > $SCRIPT_DIR/../data/db_backup/$file
