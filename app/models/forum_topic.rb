4bcb9c5e054937641c87f0f37b6b6b196d613ccf
Donovan Daniels <hewwo@yiff.rocks>
2024-12-15 11:14:04 -0600

    Prevent logging forum_topic_update for creator

--- app/models/forum_topic.rb
+++ app/models/forum_topic.rb
@@ -90,7 +90,7 @@ class ForumTopic < ApplicationRecord
         ModAction.log!(:forum_topic_move, self, forum_topic_title: title, user_id: creator_id, forum_category_id: category_id, forum_category_name: category.name, old_forum_category_id: category_id_before_last_save, old_forum_category_name: ForumCategory.find_by(id: category_id_before_last_save)&.name || "")
       end
 
-      unless specific || previously_new_record?
+      unless specific || previously_new_record? || CurrentUser.id == creator_id
         ModAction.log!(:forum_topic_update, self, forum_topic_title: title, user_id: creator_id)
       end
     end
