546da8830924e58348e59813d898b28dad809550
Donovan Daniels <hewwo@yiff.rocks>
2025-01-27 16:29:58 -0600

    Add git hash to footer

--- app/logical/git_helper.rb
+++ app/logical/git_helper.rb
@@ -9,13 +9,31 @@ module GitHelper
     else
       @hash = ""
     end
+
+    @public_hash = `git merge-base internal/master upstream/master`.strip
+  end
+
+  def self.hash
+    @hash
   end
 
   def self.short_hash
-    @hash[0..8]
+    hash[0..8]
   end
 
   def self.commit_url(commit_hash)
     "#{FemboyFans.config.source_code_url}/commit/#{commit_hash}"
+  end
+
+  def self.current_commit_url
+    commit_url(@public_hash)
+  end
+
+  def self.public_hash
+    @public_hash
+  end
+
+  def self.short_public_hash
+    public_hash[0..8]
   end
 end
