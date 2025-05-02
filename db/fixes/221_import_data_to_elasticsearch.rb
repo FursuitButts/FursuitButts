c107ccf0eecb696ac57a92ab1637a30811623239
Donovan Daniels <hewwo@yiff.rocks>
2025-05-02 16:29:58 -0500

    Add fixer to create index & import data

---
+++ db/fixes/221_import_data_to_elasticsearch.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "config", "environment"))
+
+Post.document_store.create_index!(delete_existing: true)
+Post.document_store.import
+PostVersion.document_store.create_index!(delete_existing: true)
+PostVersion.document_store.import
