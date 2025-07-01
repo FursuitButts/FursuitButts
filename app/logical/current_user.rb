af018f1a74fc8f007bc0da923afc166d7499ad62
Donovan Daniels <hewwo@yiff.rocks>
2025-06-30 23:27:58 -0500

    backport rails 7.2 TODOs

--- app/logical/current_user.rb
+++ app/logical/current_user.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
 class CurrentUser < ActiveSupport::CurrentAttributes
-  attribute(:user, :ip_addr, :request)
-  attribute(:safe_mode, default: -> { FemboyFans.config.safe_mode? })
+  attribute(:user, :ip_addr, :request, :safe_mode)
 
   alias safe_mode? safe_mode
   delegate(:id, to: :user, allow_nil: true)
   delegate_missing_to(:user)
+
+
+
+  # TODO: replace with defaults with rails 7.2 upgrade
+  def initialize
+    super
+    reset
+  end
+
+  after_reset do
+    attributes[:safe_mode] = FemboyFans.config.safe_mode?
+  end
 
   def self.scoped(user, ip_addr = "127.0.0.1", &)
     set(user: user, ip_addr: ip_addr, &)
