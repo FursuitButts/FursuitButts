FursuitButts/FursuitButts

commit 60a53070c23f0283eb99ca60fa29040fd1718291

Apparently we're particular about spacing

app/presenters/post_presenter.rb
@@ -185,7 +185,7 @@ class PostPresenter < Presenter
   end
 
   def tag_set_presenter
-    @tag_set_presenter ||= TagSetPresenter.new(@post.tag_array)
+    @tag_set_presenter ||= TagSetPresenter.new(@post.tag_array, @post)
   end
 
   def preview_html

app/presenters/tag_set_presenter.rb
@@ -8,13 +8,14 @@
 class TagSetPresenter < Presenter
   include(Rails.application.routes.url_helpers)
 
-  attr_reader(:tag_names)
+  attr_reader(:tag_names, :post)
 
   # @param [Array<String>] tag_names a list of tags to present. Tags will be presented in
   # the order given. The list should not contain duplicates. The list may
   # contain tags that do not exist in the tags table, such as metatags.
-  def initialize(tag_names)
+  def initialize(tag_names, post = nil)
     @tag_names = tag_names
+    @post = post
   end
 
   def post_index_sidebar_tag_list_html(followed_tags:, current_query: "")
@@ -106,7 +107,13 @@ class TagSetPresenter < Presenter
   end
 
   def tags_by_category
-    @tags_by_category ||= ordered_tags.group_by(&:category)
+    @tags_by_category ||= if post.nil?
+                              ordered_tags.group_by(&:category)
+                            else
+                              typed = post.typed_tags
+                              typed.map { |name| Tag.new(name: name.first).freeze }.group_by { |tag| typed[tag.name] }
+                            end
+
   end
 
   def tags_for_category(category_name)

app/views/static/site_maps/_main.xml.erb
@@ -62,4 +62,5 @@
     <% xml.lastmod(now.utc.iso8601) %>
   <% end %>
 <% end %>
+
 <%= xml.target!.html_safe %>

app/views/static/site_maps/_posts.xml.erb
@@ -1,7 +1,6 @@
 <%# locals: (from:, to:) -%>
 <% posts = Post.where(id: from..to).includes(:media_asset).not_deleted %>
 <% ViewCountCache.add_all!(Reports.get_bulk_post_views(posts.map(&:id), unique: false), :total) %>
-
 <% xml = Builder::XmlMarkup.new(indent: 2) %>
 <% xml.instruct!(:xml, version: "1.0", encoding: "UTF-8") %>
 <% xml.urlset("xmlns": "http://www.sitemaps.org/schemas/sitemap/0.9", "xmlns:image": "http://www.google.com/schemas/sitemap-image/1.1", "xmlns:video": "http://www.google.com/schemas/sitemap-video/1.1") do %>
@@ -30,12 +29,13 @@
           <% xml.video(:family_friendly, "no") %>
           <% xml.video(:uploader, post.uploader_name, info: user_url(post.uploader_id)) %>
           <% xml.video(:live, "no") %>
-          <% post.artist_tags.select(:name).limit(32).each do |tag| %>
-            <% xml.video(:tag, tag.name) %>
+          <% post.artist_tag_array.first(32).each do |tag| %>
+            <% xml.video(:tag, tag) %>
           <% end %>
         <% end %>
       <% end %>
     <% end %>
   <% end %>
 <% end %>
+
 <%= xml.target!.html_safe %>

commit 4ad5392fd6e269f2e213c55afaff3b80f91a28a1

Fix regression in 60a53070c23f0283eb99ca60fa29040fd1718291 Broke the tag list category & count

app/presenters/post_presenter.rb
@@ -185,7 +185,7 @@ class PostPresenter < Presenter
   end
 
   def tag_set_presenter
-    @tag_set_presenter ||= TagSetPresenter.new(@post.tag_array, @post)
+    @tag_set_presenter ||= TagSetPresenter.new(@post.tag_array)
   end
 
   def preview_html

app/presenters/tag_set_presenter.rb
@@ -8,14 +8,13 @@
 class TagSetPresenter < Presenter
   include(Rails.application.routes.url_helpers)
 
-  attr_reader(:tag_names, :post)
+  attr_reader(:tag_names)
 
   # @param [Array<String>] tag_names a list of tags to present. Tags will be presented in
   # the order given. The list should not contain duplicates. The list may
   # contain tags that do not exist in the tags table, such as metatags.
-  def initialize(tag_names, post = nil)
+  def initialize(tag_names)
     @tag_names = tag_names
-    @post = post
   end
 
   def post_index_sidebar_tag_list_html(followed_tags:, current_query: "")
@@ -107,12 +106,7 @@ class TagSetPresenter < Presenter
   end
 
   def tags_by_category
-    @tags_by_category ||= if post.nil?
-                            ordered_tags.group_by(&:category)
-                          else
-                            typed = post.typed_tags
-                            typed.map { |name| Tag.new(name: name.first).freeze }.group_by { |tag| typed[tag.name] }
-                          end
+    @tags_by_category ||= ordered_tags.group_by(&:category)
   end
 
   def tags_for_category(category_name)